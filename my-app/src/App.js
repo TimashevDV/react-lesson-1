28aa8d76fdd59a1ae9c15136e5f56e0095b780fc
Dmitry <Timashev_Dima@mail.ru>
2023-12-06 22:22:58 +0400

    no JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,70 @@
 import logo from './logo.svg'; // императивный стиль
 import './App.css';
+import { createElement } from 'react';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img
-					src={logo}
-					className="App-logo"
-					alt="logo"
-				/>
-				<p>
-					Edit <code>src/App.js</code> and save to
-					reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()} year</p>
-			</header>
-		</div>
+// export const App = () => {
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img
+// 					src={logo}
+// 					className="App-logo"
+// 					alt="logo"
+// 				/>
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to
+// 					reload.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<p>{new Date().getFullYear()} year</p>
+// 			</header>
+// 		</div>
+// 	);
+// };
+
+export const App = () => {
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to	reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement(
+				'p',
+				null,
+				`${new Date().getFullYear()} year`,
+			),
+		),
 	);
-}
+};
+
 // декларативный стиль это только {new Date().getFullYear()}
-export default App;
